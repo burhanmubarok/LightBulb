444b62290a5dc76de6f5b6e6f304b09d1477eccd
burhanmubarok@gmail.com <burhanmubarok@gmail.com>
2016-07-15 17:07:21 +0700

    initial

---
+++ LightBulb/LightBulb/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LightBulb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var status = Switch.TurnOn();
+            Console.WriteLine(status);
+            Console.ReadKey();
+        }
+    }
+
+    public class Switch
+    {
+        public static string TurnOn()
+        {
+            return "Light is turned on";
+        }
+
+        public static string TurnOff()
+        {
+            return "Light is turned off";
+        }
+    }
+}
---
+++ LightBulb/LightBulb/ProgramWithInterface.cs
@@ -0,0 +1,51 @@
+﻿namespace LightBulb
+{
+    class ProgramWithInterface
+    {
+
+    }
+
+    public interface ISwitch
+    {
+        string TurnOn { get; }
+        string TurnOff { get; }
+    }
+
+    public class Fan : ISwitch
+    {
+        public string TurnOn
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public string TurnOff
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+
+    public class LightBulb : ISwitch
+    {
+        public string TurnOn
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+
+        public string TurnOff
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
